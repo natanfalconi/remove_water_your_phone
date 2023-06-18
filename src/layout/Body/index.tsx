f30a344313a0a6f5743271f54179ce99394ef6d1
natanfalconi <nfalconi65@gmail.com>
2023-06-18 17:47:19 -0300

    v1.3 - test

--- src/layout/Body/index.tsx
+++ src/layout/Body/index.tsx
@@ -1,39 +1,85 @@
-import { useRef, useState } from 'react';
+// import { useRef, useState } from 'react';
+// import './body.css';
+
+// export function Body() {
+//     const [audioPlaying, setAudioPlaying] = useState(false);
+//     const audioRef = useRef<HTMLAudioElement>(null);
+
+//     const handleButtonClick = async () => {
+//         const audioElement = audioRef.current;
+
+//         if (audioElement) {
+//             try {
+//                 if (audioPlaying) {
+//                     await audioElement.pause();
+//                 } else {
+//                     await audioElement.play();
+//                 }
+//                 setAudioPlaying(!audioPlaying);
+//             } catch (error) {
+//                 console.log('Falha ao reproduzir o áudio:', error);
+//             }
+//         }
+//     };
+
+//     return (
+//         <section id='hero'>
+//             <h1>Clique aqui para remover a Água</h1>
+
+//             <button className={audioPlaying ? 'btn active' : "btn"} onClick={handleButtonClick}>
+//                 {audioPlaying ? 'Pausar áudio' : 'Remover água'}
+//             </button>
+
+//             <audio ref={audioRef} src="static/remover-agua-do-alto-falante.mp3" />
+
+//             <div className="arrow">
+//                 <a href="#about">
+//                     <span></span>
+//                     <span></span>
+//                     <span></span>
+//                 </a>
+//             </div>
+//         </section>
+//     );
+// }
+
+import { useState } from 'react';
+import ReactPlayer from 'react-player';
 import './body.css';
 
 export function Body() {
     const [audioPlaying, setAudioPlaying] = useState(false);
-    const audioRef = useRef<HTMLAudioElement>(null);
 
-    const handleButtonClick = async () => {
-        const audioElement = audioRef.current;
+    const handleButtonClick = () => {
+        setAudioPlaying(!audioPlaying);
+    };
 
-        if (audioElement) {
-            try {
-                if (audioPlaying) {
-                    await audioElement.pause();
-                } else {
-                    await audioElement.play();
-                }
-                setAudioPlaying(!audioPlaying);
-            } catch (error) {
-                console.log('Falha ao reproduzir o áudio:', error);
-            }
-        }
+    const handleVideoEnded = () => {
+        alert('A água foi removida com sucesso!');
     };
 
     return (
         <section id='hero'>
             <h1>Clique aqui para remover a Água</h1>
 
-            <button className={audioPlaying ? 'btn active' : "btn"} onClick={handleButtonClick}>
+            <button className={audioPlaying ? 'btn active' : 'btn'} onClick={handleButtonClick}>
                 {audioPlaying ? 'Pausar áudio' : 'Remover água'}
             </button>
 
-            <audio ref={audioRef} src="static/remover-agua-do-alto-falante.mp3" />
+            {audioPlaying && (
+                <ReactPlayer
+                    url='https://www.youtube.com/watch?v=Bg5pRdb3fg4'
+                    playing={audioPlaying}
+                    volume={1}
+                    muted={false}
+                    width='0'
+                    height='0'
+                    onEnded={handleVideoEnded}
+                />
+            )}
 
-            <div className="arrow">
-                <a href="#about">
+            <div className='arrow'>
+                <a href='#about'>
                     <span></span>
                     <span></span>
                     <span></span>
@@ -42,9 +88,3 @@ export function Body() {
         </section>
     );
 }
-
-
-
-
-
-
