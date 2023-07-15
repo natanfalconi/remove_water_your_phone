670cd9c8c57f8308d9355ddc5d786c133fcbfafb
Natan Falconi <nfalconi65@gmail.com>
2023-07-15 17:39:05 -0300

    change pwa builder

---
+++ public/pwabuilder-adv-sw.js
@@ -0,0 +1,3 @@
+
+import { precacheAndRoute } from 'workbox-precaching/precacheAndRoute';
+precacheAndRoute([{ "revision": "61b608f72743c1f9beb933a51ca3775a", "url": "offline.html" }, { "revision": "915d1cffb442a8212728dbd7974903e9", "url": "pwabuilder-sw.js" }]);
