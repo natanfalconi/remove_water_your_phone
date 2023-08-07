e6d5e1c2a6c920d4fbfb848fb73c6da77e755fbe
Natan da Rocha Falconi <63966608+natanfalconi@users.noreply.github.com>
2023-08-07 11:11:00 -0300

    table compatible brands

--- src/layout/Info/index.tsx
+++ src/layout/Info/index.tsx
@@ -3,33 +3,49 @@ import './info.css'
 
 export function Info() {
     const { t } = useTranslation()
+
     return (
         <section id='info'>
             <div>
                 <h2>{t('description.title')}</h2>
-
-                <p>
-                    {t('description.text')}
-                </p>
+                <p>{t('description.text')}</p>
             </div>
 
             <div>
-                <h2>{t('table.title')}</h2>
+                <h2>{t('titleTable')}</h2>
 
                 <table>
                     <thead>
                         <tr>
-                            <th>{t('table.thead.titleThead')}</th>
-                            <th>{t('table.thead.question')}</th>
+                            <th>{t('Marca')}</th>
+                            <th>{t('Compatível')}</th>
                         </tr>
                     </thead>
                     <tbody>
-                        {Object.keys(t('table.brands')).map((brandKey) => (
-                            <tr key={brandKey}>
-                                <td>{t(`table.brands.${brandKey}`)}</td>
-                                <td>{t('table.compatibility')}</td>
-                            </tr>
-                        ))}
+                        <tr>
+                            <td>{t('Apple')}</td>
+                            <td>{t('Sim')}</td>
+                        </tr>
+                        <tr>
+                            <td>{t('Samsung')}</td>
+                            <td>{t('Sim')}</td>
+                        </tr>
+                        <tr>
+                            <td>{t('Motorola')}</td>
+                            <td>{t('Sim')}</td>
+                        </tr>
+                        <tr>
+                            <td>{t('Xiaomi')}</td>
+                            <td>{t('Sim')}</td>
+                        </tr>
+                        <tr>
+                            <td>{t('Huawei')}</td>
+                            <td>{t('Sim')}</td>
+                        </tr>
+                        <tr>
+                            <td>{t('Google Pixel')}</td>
+                            <td>{t('Sim')}</td>
+                        </tr>
                     </tbody>
                 </table>
             </div>
