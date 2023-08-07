27341474b6f77b3bc22b5b437abb9f3d3572ef5f
Natan da Rocha Falconi <63966608+natanfalconi@users.noreply.github.com>
2023-08-07 09:44:41 -0300

    chages seo - no styles

--- src/layout/Info/index.tsx
+++ src/layout/Info/index.tsx
@@ -5,11 +5,51 @@ export function Info() {
     const { t } = useTranslation()
     return (
         <section id='info'>
-            <h2>{t('description.title')}</h2>
+            <div>
+                <h2>{t('description.title')}</h2>
 
-            <p>
-                {t('description.text')}
-            </p>
+                <p>
+                    {t('description.text')}
+                </p>
+            </div>
+
+            <div>
+                <h2>Marcas de Telefones Compatíveis</h2>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Marca</th>
+                            <th>Compatível?</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>iPhone (Apple)</td>
+                            <td>Sim</td>
+                        </tr>
+                        <tr>
+                            <td>Samsung</td>
+                            <td>Sim</td>
+                        </tr>
+                        <tr>
+                            <td>Motorola</td>
+                            <td>Sim</td>
+                        </tr>
+                        <tr>
+                            <td>Xiaomi</td>
+                            <td>Sim</td>
+                        </tr>
+                        <tr>
+                            <td>Huawei</td>
+                            <td>Sim</td>
+                        </tr>
+                        <tr>
+                            <td>Google Pixel</td>
+                            <td>Sim</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </section>
     )
 }
